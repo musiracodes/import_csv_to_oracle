fd6ebdf13983e70b659aff59e0b055dd4f9b408b
Musira <musiraallan@gmail.com>
2019-02-07 20:41:40 +0300

    Add files via upload

---
+++ import_csv.py
@@ -0,0 +1,23 @@
+# importcsv.py
+
+import cx_Oracle
+import csv
+
+con = cx_Oracle.connect('user/password@hostIP/database')
+
+print("Connected to Oracle")
+
+reader = csv.reader(open("yourfile.csv", "r"))
+next(reader)
+columns = []
+for line in reader:
+    columns.append(line)
+
+cur = con.cursor()
+for line in columns:
+    print("Inserting data")
+    insrt_stmt = "insert into TABLE (COLUMN1,COLUMN2,COLUMN3) values (:1, :2, :3)"
+    cur.execute(insrt_stmt, line)
+con.commit()
+cur.close()
+print("completed")
