e508ffa1534a6ad6af705b91e50cc8d351d90f3a
Musira <musiraallan@gmail.com>
2019-02-07 21:26:44 +0300

    Update import_csv.py

--- import_csv.py
+++ import_csv.py
@@ -8,7 +8,7 @@ con = cx_Oracle.connect('user/password@hostIP/database')
 print("Connected to Oracle")
 
 reader = csv.reader(open("yourfile.csv", "r"))
-next(reader)
+next(reader) # assumption is your db_table has header
 columns = []
 for line in reader:
     columns.append(line)
@@ -16,7 +16,7 @@ for line in reader:
 cur = con.cursor()
 for line in columns:
     print("Inserting data")
-    insrt_stmt = "insert into TABLE (COLUMN1,COLUMN2,COLUMN3) values (:1, :2, :3)"
+    insrt_stmt = "insert into TABLE (COLUMNNAME1,COLUMNNAME2,COLUMNNAME3) values (:1, :2, :3)"
     cur.execute(insrt_stmt, line)
 con.commit()
 cur.close()
